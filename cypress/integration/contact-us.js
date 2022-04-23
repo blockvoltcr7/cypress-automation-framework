389bb9b2a881c6a0f5fb0b45775a2f6299f6a33d
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-04-23 06:08:29 -0400

    first cypress test clicking and typing

---
+++ cypress/integration/contact-us.js
@@ -0,0 +1,21 @@
+/// <reference types="Cypress" />
+
+describe("test contact us form via webdriverUni", () => {
+
+    it("should be able to submit a successful submission via contact us form", () => {
+        
+        cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+        // cy.get('#contact-us').click()
+        cy.get('[name="first_name"]').type('Sami')
+        cy.get('[name="last_name"]').type('Sabir-Idrissi')
+        cy.get('[name="email"]').type('samisabir212@gmail.com')
+        cy.get('textarea.feedback-input').type('Cypress is super awesome and extremely fast compared to Selenium with Java')
+        cy.get('[type="submit"]').click()
+       
+    });
+
+    it("should not be able to submit a successful submission via contact us form as all fields are required", () => {
+        
+    });
+
+})
