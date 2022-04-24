888dbd1ca4e61ed0edd4edbbc7dcbbb5d7eb46c0
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-04-24 03:24:50 -0400

    adding more assertions

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -8,6 +8,7 @@ describe("Test contact us via automation test store", () => {
         cy.get('.info_links_footer > :nth-child(5) > a').click()
         cy.get('#ContactUsFrm_first_name').type('Sami')
         cy.get('#ContactUsFrm_email').type('samisabir212@gmail.com')
+        cy.get('#ContactUsFrm_email').should('have.attr','name','email')
         cy.get('#ContactUsFrm_enquiry').type('hello sami')
         cy.get('.col-md-6 > .btn').click()
         cy.get('.mb40 > :nth-child(3)').should('have.text','Your enquiry has been successfully sent to the store owner!')
--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -2,7 +2,7 @@
 
 describe("test contact us form via webdriverUni", () => {
 
-    it("should be able to submit a successful submission via contact us form", () => {
+    it.only("should be able to submit a successful submission via contact us form", () => {
         
         cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
         // cy.get('#contact-us').click()
@@ -11,10 +11,11 @@ describe("test contact us form via webdriverUni", () => {
         cy.get('[name="email"]').type('samisabir212@gmail.com')
         cy.get('textarea.feedback-input').type('Cypress is super awesome and extremely fast compared to Selenium with Java')
         cy.get('[type="submit"]').click()
+        cy.get('h1').should('have.text','Thank You for your Message!')
        
     });
 
-    it.only("should not be able to submit a successful submission via contact us form as all fields are required", () => {
+    it("should not be able to submit a successful submission via contact us form as all fields are required", () => {
         
         cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
         // cy.get('#contact-us').click()
