8bd400c608e0ef2178ad49ee2a2f71a320313248
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-04-24 02:45:54 -0400

    first assertion

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -5,9 +5,12 @@ describe("Test contact us via automation test store", () => {
     it("", () => {
         
         cy.visit('http://automationteststore.com/')
-
-     
-       
+        cy.get('.info_links_footer > :nth-child(5) > a').click()
+        cy.get('#ContactUsFrm_first_name').type('Sami')
+        cy.get('#ContactUsFrm_email').type('samisabir212@gmail.com')
+        cy.get('#ContactUsFrm_enquiry').type('hello sami')
+        cy.get('.col-md-6 > .btn').click()
+        cy.get('.mb40 > :nth-child(3)').should('have.text','Your enquiry has been successfully sent to the store owner!')
     });
 
 
