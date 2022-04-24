c0b88354230cfc594a973b50f008f3438e83c033
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-04-24 03:34:17 -0400

    validating document charset

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -2,10 +2,10 @@
 
 describe("test contact us form via webdriverUni", () => {
 
-    it("should be able to submit a successful submission via contact us form", () => {
+    it.only("should be able to submit a successful submission via contact us form", () => {
         
         cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
-        // cy.get('#contact-us').click()
+        cy.document().should('have.property','charset').and('eq','UTF-8')
         cy.get('[name="first_name"]').type('Sami')
         cy.get('[name="last_name"]').type('Sabir-Idrissi')
         cy.get('[name="email"]').type('samisabir212@gmail.com')
@@ -15,7 +15,7 @@ describe("test contact us form via webdriverUni", () => {
        
     });
 
-    it.only("should not be able to submit a successful submission via contact us form as all fields are required", () => {
+    it("should not be able to submit a successful submission via contact us form as all fields are required", () => {
         
         cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
         // cy.get('#contact-us').click()
