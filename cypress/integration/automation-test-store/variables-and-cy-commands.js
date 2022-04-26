117d1bcda8c497cdfe27d50781edf7956359b42f
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-04-26 04:24:55 -0400

    utilizing different ways to get the element text and assert the value with promises, chaining, and closures

--- cypress/integration/automation-test-store/variables-and-cy-commands.js
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -25,6 +25,30 @@ describe("Variables verfied, cypress commands and jquery commands", () => {
 
     });
 
+    it.only("Validate properties of the Contact Us Page", () => {
+        
+
+        cy.visit('https://automationteststore.com/index.php?rt=content/contact')
+
+
+        //use cypress commands and chaining (checking for multiple elements)
+
+        //find the contact form, check if it has 'contact us form' then find the field
+        cy.contains('#ContactUsFrm', 'Contact Us Form').find('#field_11').should('contain','First name')
+
+        //Jqquery Approach
+        cy.contains('#ContactUsFrm', 'Contact Us Form').then(text => {
+            const firstNameText = text.find('#field_11').text()
+            expect(firstNameText).to.contain('First name')
+        })
+
+        //Embeded commands (closure)
+        cy.get('#field_11').then(fnText => {
+            cy.log(fnText.text())
+            cy.log(fnText)
+        })
+
+    });  
 
 
 })
